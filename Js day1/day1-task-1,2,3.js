6711e2c26f9ab966d741da8c1707fbbece916e81
prasanth2433 <24bct135@kgcas.com>
2025-07-12 10:45:37 +0530

    Add files via upload

---
+++ Js day1/day1-task-1,2,3.js
@@ -0,0 +1,21 @@
+//task1
+var number=12;
+var name=`java script`;
+var iscloudy=true;
+
+console.log(number+ " "+name+" "+iscloudy);
+document.write(number+ " "+name+" "+iscloudy);
+
+//task2
+console.log(typeof(number));
+console.log(typeof(name));
+console.log(typeof(iscloudy));
+
+//task3
+var num1=parseInt(prompt("Enter the first value"));
+var num2=parseInt(prompt("Enter the second value"));
+alert(num1+num2)
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
